e9db4a848ce99b0a4033409491fefce3c4cf7c7c
RYAN ALCÂNTARA DOS SANTOS <25203781@dsn127m1242374.cfp127aluno.local>
2026-09-17 14:28:57 -0300

    android enemy

--- app/src/main/java/com/example/componentesestado/MainActivity.kt
+++ app/src/main/java/com/example/componentesestado/MainActivity.kt
@@ -6,20 +6,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.componentesestado.ui.theme.ComponentesEstadoTheme
+import com.example.componentesestado.ui.theme.fonte
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +49,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BasicComponentsScreen(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
             .background(Color(239, 247, 207))
     ) {
 
@@ -49,14 +59,45 @@ fun BasicComponentsScreen(modifier: Modifier = Modifier) {
             fontSize = 32.sp,
             fontWeight = FontWeight.Bold,
             color = Color(0xFF3DDC84),
-            fontFamily = FontFamily.Serif
+            fontFamily = FontFamily.Serif,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.End,
+            letterSpacing = 4.sp
         )
         Text(
             text = "Com jetpack compose",
             fontSize = 16.sp,
             fontWeight = FontWeight.SemiBold,
-            color = Color(66, 137, 244))
+            color = Color(66, 137, 244),
+            fontFamily = fonte,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
 
+        )
+
+        AndroidEnemy(
+            modifier = Modifier.size(100.dp),
+            color = Color.Red
+        )
+
+        AndroidEnemy(
+            modifier = Modifier.size(100.dp),
+            color = Color.Black
+        )
     }
+
+}
+
+@Composable
+fun AndroidEnemy(
+    color: Color,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(R.drawable.ic_launcher_foreground),
+        colorFilter = ColorFilter.tint(color = color),
+        contentDescription = "Android Enemy"
+    )
 
 }
--- app/src/main/java/com/example/componentesestado/ui/theme/Type.kt
+++ app/src/main/java/com/example/componentesestado/ui/theme/Type.kt
@@ -2,11 +2,15 @@ package com.example.componentesestado.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.componentesestado.R
 
-val fonte = FontFamily()
+val fonte = FontFamily(
+    Font(R.font.righteous_regular)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
