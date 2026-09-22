2d4e36999fa3536f2ef37826a958d8f103df21e4
RYAN ALCÂNTARA DOS SANTOS <25203781@dsn127m1242374.cfp127aluno.local>
2026-09-22 16:56:03 -0300

    label

--- app/src/main/java/com/example/componentesestado/MainActivity.kt
+++ app/src/main/java/com/example/componentesestado/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.componentesestado
 import android.R.attr.fontWeight
 import android.R.attr.text
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,9 +14,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,6 +28,8 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,6 +55,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicComponentsScreen(modifier: Modifier = Modifier) {
+    val textoInput = remember {
+        mutableStateOf("batatal")
+    }
+
+    val quantidade = remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -75,15 +90,43 @@ fun BasicComponentsScreen(modifier: Modifier = Modifier) {
 
         )
 
-        AndroidEnemy(
-            modifier = Modifier.size(100.dp),
-            color = Color.Red
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = textoInput.value,
+            onValueChange = { novoValor ->
+                Log.i("teste", novoValor)
+
+                textoInput.value = novoValor
+            },
+
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words
+            ),
+
+            label = {
+                Text("Nome e sobrenome")
+            }
         )
 
-        AndroidEnemy(
-            modifier = Modifier.size(100.dp),
-            color = Color.Black
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = quantidade.value,
+            onValueChange = { novoValor ->
+                quantidade.value = novoValor
+            },
+
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            ),
+            placeholder = {
+                Text("Qual a quantidade?")
+            }
+
+
+
         )
+
+
     }
 
 }
